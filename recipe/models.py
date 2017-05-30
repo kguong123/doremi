1c037a0d2cb88e347159a844e69a8290e97ea43d
sujin han <sujin han>
2017-05-30 19:39:20 +0900

    Create 5 recipe posts

--- recipe/models.py
+++ recipe/models.py
@@ -49,8 +49,8 @@ class Recipe(models.Model):
 @python_2_unicode_compatible
 class Foodinfo(models.Model):
     foodinfo = models.ForeignKey(Recipe)
-    ingredient = models.CharField(max_length=200)
-    quantity = models.CharField(max_length=200, help_text='재료량: 포기, 개, 공기')
+    ingredient = models.CharField(max_length=200,blank=True)
+    quantity = models.CharField(max_length=200,blank=True, help_text='재료량: 포기, 개, 공기')
 
     def __str__(self):
         return self.ingredient
